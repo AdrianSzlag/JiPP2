297bd8975fc0d471741a54958e07cbe6943ef988
AdrianSzlag <92638498+AdrianSzlag@users.noreply.github.com>
2021-10-18 14:24:37 +0200

    Lab 2
    Zajęcia 18.10.2021

---
+++ firstLib/src/lib.cpp
@@ -0,0 +1,11 @@
+//
+// Created by ADR on 18.10.2021.
+//
+#include <iostream>
+#include "../include/lib.h"
+
+using namespace std;
+
+void itIsWork(){
+    cout << "Yes it work!" << endl;
+}
---
+++ firstLibCall/src/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by ADR on 18.10.2021.
+//
+#include "lib.h"
+
+int main(){
+    itIsWork();
+    return 0;
+}
