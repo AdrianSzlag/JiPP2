04eea97d615f99761bf149b3c626ea1e5b8fde52
AdrianSzlag <92638498+AdrianSzlag@users.noreply.github.com>
2021-12-12 17:41:24 +0100

    lab9

---
+++ lab9/src/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> vect;
+    cout<< "Size: " << vect.size() << " \tCapacity: " << vect.capacity() << endl;
+
+    vect.push_back(1);
+    cout<< "Size: " << vect.size() << " \tCapacity: " << vect.capacity() << endl;
+
+    vect.push_back(1);
+    cout<< "Size: " << vect.size() << " \tCapacity: " << vect.capacity() << endl;
+
+    vect.push_back(1);
+    vect.push_back(1);
+    vect.push_back(1);
+    vect.push_back(1);
+    vect.push_back(1);
+    vect.push_back(1);
+    vect.push_back(1);
+    vect.push_back(1);
+    vect.push_back(1);
+    vect.push_back(1);
+    cout<< "Size: " << vect.size() << " \tCapacity: " << vect.capacity() << endl;
+
+    vect.pop_back();
+    cout<< "Size: " << vect.size() << " \tCapacity: " << vect.capacity() << endl;
+
+    vect.pop_back();
+    cout<< "Size: " << vect.size() << " \tCapacity: " << vect.capacity() << endl;
+
+    vect.pop_back();
+    vect.pop_back();
+    vect.pop_back();
+    vect.pop_back();
+    vect.pop_back();
+    cout<< "Size: " << vect.size() << " \tCapacity: " << vect.capacity() << endl;
+
+    return 0;
+}
